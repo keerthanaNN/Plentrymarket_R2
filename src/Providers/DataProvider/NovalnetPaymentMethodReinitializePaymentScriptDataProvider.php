a6cd0da46257fce52194759ffd2af2079398a5d5
keerthanaNN <118429149+keerthanaNN@users.noreply.github.com>
2022-11-18 12:10:35 +0530

    Add files via upload

---
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentScriptDataProvider.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is used for customer reinitialize payment process
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet
+ * @license      https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+namespace Novalnet\Providers\DataProvider;
+
+use Plenty\Plugin\Templates\Twig;
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+use Plenty\Modules\Payment\Method\Models\PaymentMethod;
+
+/**
+ * Class NovalnetPaymentMethodReinitializePaymentScriptDataProvider
+ *
+ * @package Novalnet\Providers\DataProvider
+ */
+class NovalnetPaymentMethodReinitializePaymentScriptDataProvider
+{
+    /**
+     * Script for displaying the reinitiate payment button
+     *
+     * @param Twig $twig
+     *
+     * @return string
+     */
+    public function call(Twig $twig)
+    {
+        // Load the all Novalnet payment methods
+        $paymentMethodRepository = pluginApp(PaymentMethodRepositoryContract::class);
+        $paymentMethods          = $paymentMethodRepository->allForPlugin('plenty_novalnet');
+        if(!is_null($paymentMethods)) {
+            $paymentMethodIds              = [];
+            $nnPaymentMethodKey = $nnPaymentMethodId = '';
+            foreach($paymentMethods as $paymentMethod) {
+                if($paymentMethod instanceof PaymentMethod) {
+                    $paymentMethodIds[] = $paymentMethod->id;
+                    if($paymentMethod->paymentKey == 'NOVALNET_APPLEPAY') {
+                        $nnPaymentMethodKey = $paymentMethod->paymentKey;
+                        $nnPaymentMethodId = $paymentMethod->id;
+                    }
+                }
+            }
+        }
+        return $twig->render('Novalnet::NovalnetPaymentMethodReinitializePaymentScriptDataProvider',
+                                    [
+                                        'paymentMethodIds'      => $paymentMethodIds,
+                                        'nnPaymentMethodKey'    => $nnPaymentMethodKey,
+                                        'nnPaymentMethodId'     => $nnPaymentMethodId
+                                    ]);
+    }
+}
